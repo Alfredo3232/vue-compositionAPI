4183689cbecaa48c6c297e0dbd23ca3c79441282
Alfredo3232 <61888719+Alfredo3232@users.noreply.github.com>
2022-12-12 16:16:19 -0600

    move user name functionality to UserData

--- src/App.vue
+++ src/App.vue
@@ -1,35 +1,53 @@
 <template>
   <section class="container">
-    <h2>{{ uName }}</h2>
-    <h3>{{ uAge }}</h3>
+    <user-data :first-name="firstName" :last-name="lastName" :age="uAge"></user-data>
 
     <button @click="setNewAge">Change Age</button>
 
     <div>
       <input type="text" placeholder="First Name" v-model="firstName" />
-      <input type="text" placeholder="Last Name" v-model="lastName" />
+      <input type="text" placeholder="Last Name" ref="lastNameInput" />
+      <button @click="setLastName">Set Last Name</button>
     </div>
   </section>
 </template>
 
 <script>
-import { ref, computed } from 'vue';
+import { ref, computed, watch } from "vue";
+import UserData from "./components/UserData.vue";
 
 export default {
+  components: {
+    'user-data': UserData
+  },
   setup() {
     // Data
-    let firstName = ref('');
-    let lastName = ref('');
+    let firstName = ref("");
+    let lastName = ref("");
+    let lastNameInput = ref(null);
     let uAge = ref(21);
 
     // Computed
     let uName = computed(() => {
-      return firstName.value + ' ' + lastName.value;
+      return firstName.value + " " + lastName.value;
+    });
+
+    // Watch
+    watch([uAge, uName], function (newVals, oldVals) {
+      console.log("Old age: " + oldVals[0]);
+      console.log("New age: " + newVals[0]);
+
+      console.log("Old name: " + oldVals[1]);
+      console.log("New name: " + newVals[1]);
     });
 
     // Methods
     function setNewAge() {
       uAge.value = 22;
+    }
+
+    function setLastName() {
+      lastName.value = lastNameInput.value.value;
     }
 
     // Return
@@ -38,7 +56,9 @@ export default {
       uAge,
       setNewAge,
       firstName,
-      lastName
+      lastName,
+      lastNameInput,
+      setLastName
     };
   }
 };
---
+++ src/components/UserData.vue
@@ -0,0 +1,15 @@
+<template>
+    <h2>{{ userName }}</h2>
+    <h3>{{ uAge }}</h3>
+</template>
+
+<script>
+export default {
+    props: ['firstName', 'lastName', 'uAge'],
+    computed: {
+        userName() {
+            return this.firstName + ' ' + this.lastName;
+        }
+    }
+};
+</script>
