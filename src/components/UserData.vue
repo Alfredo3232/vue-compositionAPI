5ca797999f4ed6d894371aa6819d8267ce1fd8a1
Alfredo3232 <61888719+Alfredo3232@users.noreply.github.com>
2022-12-12 16:34:40 -0600

    refactor UserData to use composition API

--- src/components/UserData.vue
+++ src/components/UserData.vue
@@ -4,12 +4,25 @@
 </template>
 
 <script>
+import { computed } from 'vue';
+
 export default {
     props: ['firstName', 'lastName', 'uAge'],
-    computed: {
-        userName() {
-            return this.firstName + ' ' + this.lastName;
+    setup(props, context) {
+        const userName = computed(() => {
+            return props.firstName + ' ' + props.lastName;
+        });
+
+        // context.$emit('save-data', 1);
+
+        return {
+            userName
         }
     }
+    // computed: {
+    //     userName() {
+    //         return this.firstName + ' ' + this.lastName;
+    //     }
+    // }
 };
 </script>
