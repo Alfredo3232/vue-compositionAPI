48ed00ed15f654ec670c98c234571ca0532b9189
Alfredo3232 <61888719+Alfredo3232@users.noreply.github.com>
2022-12-13 18:02:31 -0600

    add console logs to on functions

--- src/components/UserData.vue
+++ src/components/UserData.vue
@@ -4,20 +4,52 @@
 </template>
 
 <script>
-import { computed, inject } from 'vue';
+import {
+    computed,
+    inject,
+    onBeforeMount,
+    onMounted,
+    onBeforeUpdate,
+    onUpdated,
+    onBeforeUnmount,
+    onUnmounted
+} from 'vue';
 
 export default {
     props: ['firstName', 'lastName'],
     setup(props, context) {
+        // computed 
         const userName = computed(() => {
             return props.firstName + ' ' + props.lastName;
         });
 
+        // inject
         const uAge = inject('userAge');
 
         console.log(context);
         // context.$emit('save-data', 1);
 
+        // on functions
+        onBeforeMount(() => {
+            console.log("onBeforeMount");
+        });
+        onMounted(() => {
+            console.log("onMounted");
+        });
+        onBeforeUpdate(() => {
+            console.log("onBeforeUpdate");
+        });
+        onUpdated(() => {
+            console.log("onUpdated");
+        });
+        onBeforeUnmount(() => {
+            console.log("onBeforeUnmount");
+        });
+        onUnmounted(() => {
+            console.log("onUnmounted");
+        });
+
+        // return props and funcs
         return {
             userName,
             uAge
